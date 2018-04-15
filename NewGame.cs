c28b9c8765ad1e01f78d165602409d148c25af3b
codyfitch <35245717+codyfitch@users.noreply.github.com>
2018-04-14 23:30:07 -0600

    Main Menu with end game return

---
+++ NewGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+
+    public void LoadStage()
+    {
+        SceneManager.LoadScene("Done_Main");
+    }
+}
